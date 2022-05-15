568a89aa4cc6ee9656b05d5f3619a7726007044d
Arijit Ray <35370469+itsarijitray@users.noreply.github.com>
2022-05-15 23:20:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const linkInfo={
+    "github":"https://github.com/itsarijitray",
+    "linkedin":"https://www.linkedin.com/in/itsarijitray/",
+    "twitter":"https://twitter.com/itsarijitray",
+    "facebook":"https://www.facebook.com/itsarijitray",
+    "instagram":"https://www.instagram.com/itsarijitray/",
+    "rotaract":"https://www.facebook.com/rco.altruism",
+    "mail":"mailto: arijitray.official@gmail.com"
+}
+
+
+const onHandleClick = (link) => {
+    window.location.href = linkInfo[link]
+}
